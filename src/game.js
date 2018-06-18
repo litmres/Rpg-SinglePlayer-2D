3f5e6e5d8641560a98010e1411848b1f99d89551
twofist <twofist12345@gmail.com>
2018-06-18 15:15:35 +0200

    can finally start now i think

--- src/game.js
+++ src/game.js
@@ -1,20 +1,25 @@
-class SimpleGame {
-    constructor() {
+"use strict";
+var SimpleGame = /** @class */ (function () {
+    function SimpleGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create });
+        this.loadingText = this.game.add.text(0, 0, "", {
+            fill: "#ffffff",
+        });
     }
-    preload() {
+    SimpleGame.prototype.preload = function () {
         this.game.load.onLoadStart.add(assets, this);
         this.game.load.image("logo", "assets/ds_logo.png");
         this.game.load.onFileComplete.add(progressBar, this);
         this.game.load.onLoadComplete.add(finishedLoading, this);
-    }
-    create() {
-        const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
+    };
+    SimpleGame.prototype.create = function () {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "logo");
         logo.anchor.setTo(0.5, 0.5);
-    }
-}
-window.onload = () => {
-    const game = new SimpleGame();
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
 };
 function progressBar(progress, cacheKey, success, totalLoaded, totalFiles) {
     this.loadingText.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
@@ -28,4 +33,3 @@ function assets() {
 function finishedLoading() {
     this.loadingText.setText("Load Complete");
 }
-//# sourceMappingURL=game.js.map
--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,12 @@
 class SimpleGame {
-	private game: Phaser.Game;
-	private loadingText:Phaser.Text;
+	public game: Phaser.Game;
+	public loadingText:Phaser.Text;
 	
 	constructor() {
 		this.game = new Phaser.Game( 800, 600, Phaser.AUTO, "content", { preload: this.preload, create: this.create} );
+		this.loadingText = this.game.add.text(0, 0, "", {
+			fill: "#ffffff",
+		});
 	}
 	
 	private preload() {
@@ -11,7 +14,6 @@ class SimpleGame {
 		this.game.load.image( "logo", "assets/ds_logo.png" );
 		this.game.load.onFileComplete.add(progressBar, this);
 		this.game.load.onLoadComplete.add(finishedLoading, this);
-
 	}
 	
 	private create() {
@@ -24,17 +26,17 @@ window.onload = () => {
 	const game = new SimpleGame();
 };
 
-function progressBar(progress, cacheKey, success, totalLoaded, totalFiles) {
+function progressBar(this:SimpleGame, progress:Phaser.Loader, cacheKey:Phaser.Cache, success:Phaser.Signal, totalLoaded:Phaser.Loader, totalFiles:Phaser.Loader) {
 	this.loadingText.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
 }
 
-function assets() {
+function assets(this:SimpleGame) {
 	this.loadingText = this.game.add.text(this.game.camera.x, this.game.camera.height / 2, "loading...", {
 		fill: "#ffffff",
 	});
 	this.game.stage.backgroundColor = 0xB20059;
 }
 
-function finishedLoading() {
+function finishedLoading(this:SimpleGame) {
 	this.loadingText.setText("Load Complete");
 }
